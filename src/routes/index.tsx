925d63147a0636961128d37fff0bdb1ad25eb733
GustavoTagli <taglisouza.0304@hotmail.com>
2022-10-05 13:09:04 -0300

    Mobile aplication complete [v1.0]

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoute } from './app.routes';
+
+export function Routes(){
+    return (
+        <NavigationContainer>
+            <AppRoute />
+        </NavigationContainer>
+    )
+}
